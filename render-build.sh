e1b1df63e0e7df396e3bae9aec4ac1c2ac509b76
Joselots <gonzalezortegajoselo06@gmail.com>
2025-01-10 10:42:01 -0600

    Añadiendo script para instalar dependencias en Render

---
+++ render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y \
+    gobject-introspection \
+    libgirepository1.0-dev \
+    libcairo2-dev \
+    pkg-config \
+    python3-dev \
+    libglib2.0-dev
+
+
